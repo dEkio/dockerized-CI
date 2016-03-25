d5a00d39468c16bdf6ce83d5dad5c8cb84bbaddf
Dejan Angelevski <Dejan.Angelevski@endava.com>
2016-03-25 13:35:03 +0100

    updated groovy script

--- jenkins/seedjobs/githubSeedJobs.groovy
+++ jenkins/seedjobs/githubSeedJobs.groovy
@@ -2,7 +2,7 @@ def githubApi = new URL("https://api.github.com/users/dEkio/repos")
 def projects = new groovy.json.JsonSlurper().parse(githubApi.newReader())
 
 projects.each {
-  def jobName=it.name
+  def jobName=initial
   def githubName=it.full_name
   def gitUrl=it.ssh_url
   println "Creating Job ${jobName} for ${gitUrl}"
@@ -16,14 +16,39 @@ projects.each {
         githubPush()
     }
   }
-}
+  
+  def jobName1=dev
+  println "Creating Job ${jobName1} for ${gitUrl}"
 
-listView('MB GitHub Jobs') {
+  job("GitHub-${jobName}") {
+    logRotator(-1, 10)
+    scm {
+        github(githubName, 'master')
+    }
+    triggers {
+        githubPush()
+    }
+  }
+  
+  def jobName2=test
+  println "Creating Job ${jobName2} for ${gitUrl}"
+
+  job("GitHub-${jobName}") {
+    logRotator(-1, 10)
+    scm {
+        github(githubName, 'master')
+    }
+    triggers {
+        githubPush()
+    }
+  }
+
+  listView(githubName) {
     description('')
     filterBuildQueue()
     filterExecutors()
     jobs {
-        regex(/GitHub-.*/)
+        regex(/${githubName}-.*/)
     }
     columns {
         status()
@@ -34,4 +59,6 @@ listView('MB GitHub Jobs') {
         lastFailure()
         lastDuration()
     }
+}
+  
 }
