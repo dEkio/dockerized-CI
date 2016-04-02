b7f90fb6f4de8e923fd3019bb4fbdbdf4e890ebe
Dejan Angelevski <dejan.angelevski@endava.com>
2016-04-02 19:29:25 +0200

    new groovy script

--- jenkins/seedjobs/eurekaDemo.groovy
+++ jenkins/seedjobs/eurekaDemo.groovy
@@ -1,11 +1,12 @@
-def gitUrl="https://github.com/marcelbirkner/docker-ci-tool-stack"
+def gitUrl="git@github.com:dEkio/microservicesLocalMonolith.git"
 
-createDockerJob("docker-admin-version", "sudo /usr/bin/docker version", "")
-createDockerJob("docker-admin-list-running-container", "sudo /usr/bin/docker ps", "")
-createDockerJob("docker-admin-list-images", "sudo /usr/bin/docker images", "")
-createDockerJob("docker-admin-build-jenkins-container", "cd jenkins && sudo /usr/bin/docker build -t jenkins .", gitUrl)
-// createDockerJob("docker-admin-start-jenkins-container", "sudo /usr/bin/docker run -d --name did_jenkins -p=28080:8080 jenkins", gitUrl)
-// createDockerJob("docker-admin-stop-jenkins-container", 'sudo /usr/bin/docker stop \$(sudo /usr/bin/docker ps -a -q --filter="name=did_jenkins") && sudo /usr/bin/docker rm \$(sudo /usr/bin/docker ps -a -q --filter="name=did_jenkins")', "")
+List<String> deployToContainerCommands = [ "sudo /usr/bin/docker stop eureka1 || echo 'no container to stop'", "sudo /usr/bin/docker rm eureka1 || echo 'no container to delete'", "cd eureka && sudo /usr/bin/docker build -t eureka .", "sudo /usr/bin/docker run -d --name eureka1 -p=18761:8761 eureka"]
+List<String> deployToNexusCommands = [ "mvn -Drepository.address=dockerizedci_nexus_1:8081 -f ${WORKSPACE}/eureka/pom.xml clean deploy" ]
+List<String> initial = [ "mvn  -f ${WORKSPACE}/eureka/pom.xml clean install"]
+
+createDockerJob("eureka-initial", initial, gitUrl)
+createDockerJob("eureka-deploy_artefact_nexus", deployToNexusCommands, gitUrl)
+createDockerJob("eureka-deploy_jar_container", deployToContainerCommands, gitUrl)
 
 def createDockerJob(def jobName, def shellCommand, def gitRepository) {
 
@@ -30,7 +31,9 @@ def createDockerJob(def jobName, def shellCommand, def gitRepository) {
     }
     steps {
       steps {
-        shell(shellCommand)
+		shellCommand.each{
+			shell(shellCommand)
+		}
       }
     }
     publishers {
@@ -39,12 +42,12 @@ def createDockerJob(def jobName, def shellCommand, def gitRepository) {
   }
 }
 
-listView('admin docker') {
+listView('eureka service') {
   description('')
   filterBuildQueue()
   filterExecutors()
   jobs {
-    regex(/docker-admin-.*/)
+    regex(/eureka-.*/)
   }
   columns {
     buildButton()
